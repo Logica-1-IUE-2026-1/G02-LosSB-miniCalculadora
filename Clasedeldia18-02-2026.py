b7011bad5e4634ea169f54714c8a2ec71c46cc51
aldanabuelvasireidis-hub <aldanabuelvasireidis@gmail.com>
2026-02-22 21:33:20 -0500

    Create Clasedeldia18-02-2026.py
    Documento py de la clase 18-02-2026

---
+++ Clasedeldia18-02-2026.py
@@ -0,0 +1,18 @@
+#Inicio
+const = 6
+var = int(input("escribe la variable"))
+result = var + const
+print (f"la suma de {const} + {var} es igual a {result}")
+result2 = result - const
+print (f"la resta de {result} - {const} es igual a {result2}")
+result3 = result2 * const
+print (f"la multiplicación de {result2} * {const} es igual a {result3}")
+result4 = result3 / const
+print (f"la división de {result3} / {const} es igual a {result4}")
+result5 = result4 // const
+print (f"la división entera de {result4} // {const} es igual a {result5}")
+result6 = result5 % const
+print (f"el modulo de {result5} % {const} es igual a {result6}")
+result7 = result6 ** const
+print (f"la potencia de {result6} ** {const} es igual a {result7}")
+#fin
